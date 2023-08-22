017b7b7fc18bb1921fedc7b8036d390eb24b1af9
Austin Mangum <mangumaustin@gmail.com>
2023-08-21 20:35:45 -0500

    commit

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -15,7 +15,7 @@ const navigationEvents = () => {
 
   // TODO: ALL BOOKS
   document.querySelector('#all-books').addEventListener('click', () => {
-    booksOnSale().then(showBooks);
+    getBooks().then(showBooks);
   });
 
   // FIXME: STUDENTS Create an event listener for the Authors

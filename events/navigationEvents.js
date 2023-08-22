13c25384ec6193cf92f16adb3bb010986082b080
Austin Mangum <mangumaustin@gmail.com>
2023-08-21 19:21:39 -0500

    commit

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,6 @@
 import { signOut } from '../utils/auth';
+import { booksOnSale, getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
 
 // navigation events
 const navigationEvents = () => {
@@ -8,12 +10,12 @@ const navigationEvents = () => {
 
   // TODO: BOOKS ON SALE
   document.querySelector('#sale-books').addEventListener('click', () => {
-    console.warn('CLICKED SALE BOOKS');
+    booksOnSale().then(showBooks);
   });
 
   // TODO: ALL BOOKS
   document.querySelector('#all-books').addEventListener('click', () => {
-    console.warn('CLICKED ALL BOOKS');
+    booksOnSale().then(showBooks);
   });
 
   // FIXME: STUDENTS Create an event listener for the Authors
@@ -21,7 +23,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    booksOnSale().then(showBooks);
   });
 
   // STRETCH: SEARCH
